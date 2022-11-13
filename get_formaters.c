6d4b985c7fd0de57a212b6ffb1cc1471a4fd86d4
jose tirado <joca-001@hotmail.com>
2022-11-13 12:17:13 -0800

    fix-indent

--- get_formaters.c
+++ get_formaters.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * get_formaters - Extract the formaters (flags, field width, precision
  * length modifier and conversion specifier), if any, from the format string
@@ -11,32 +10,27 @@
  */
 char *get_formaters(const char *format)
 {
-        int pos;
-        char *formaters;
+	int pos;
+	char *formater;
 
-        /* Gets the position of the first letter or percent sign */
-        pos = pos_first_ltr_or_perc(format);
-        if (pos < 0)
-                return NULL;
-
-        if (is_specificator(format[pos]))
-        {
-                formaters = malloc((pos + 2) * sizeof(char));
-                formaters = _strncpy(formaters, format, pos + 1);
-                return (formaters);
-        }
-
-        if (format[pos] == 'l' || format[pos] == 'h')
-        {
-                if (is_specificator(format[pos + 1]))
-                {
-                        formaters = malloc((pos + 3) *
-sizeof(char));
-                        formaters = _strncpy(formaters, format, pos + 2);
-                        return (formaters);
-                }
-                return (NULL);
-        }
-
-        return (NULL);
+	pos = pos_first_ltr_or_perc(format);
+	if (pos < 0)
+		return (NULL);
+	if (is_specificator(format[pos]))
+	{
+		formaters = malloc((pos + 2) * sizeof(char));
+		formaters = _strncpy(formaters, format, pos + 1);
+		return (formaters);
+	}
+	if (format[pos] == 'l' || format[pos] == 'h')
+	{
+		if (is_specificator(format[pos + 1]))
+		{
+			formaters = malloc((pos + 3) * sizeof(char));
+			formaters = _strncpy(formaters, format, pos + 2);
+			return (formaters);
+		}
+		return (NULL);
+	}
+	return (NULL);
 }
