ea790408683871ae250baebb804f61d44bb4d0d0
jose tirado <joca-001@hotmail.com>
2022-11-13 13:23:35 -0800

    add store string

--- string_storers.c
+++ string_storers.c
@@ -11,3 +11,28 @@ void store_c(va_list args, char *buffer)
 {
         *buffer = va_arg(args, int);
 }
+
+/**
+ * store_str - Stores string in buffer
+ * @args: Argument of type string
+ * @buffer: Buffer where the string will be stored
+ *
+ * Return: Nothing
+ */
+void store_str(va_list args, char *buffer)
+{
+        char *str;
+
+        str = va_arg(args, char *);
+
+        if (str == NULL)
+        {
+                str = "(null)";
+        }
+
+        for (; *str != '\0'; str++)
+        {
+                *buffer = *str;
+                buffer++;
+        }
+}
