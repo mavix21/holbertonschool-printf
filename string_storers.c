e2bc13eeb0d75857e502b3cc6cec72a2dfb989be
jose tirado <joca-001@hotmail.com>
2022-11-13 13:25:04 -0800

    add store string rev

--- string_storers.c
+++ string_storers.c
@@ -36,3 +36,35 @@ void store_str(va_list args, char *buffer)
                 buffer++;
         }
 }
+
+/**
+ * store_rev - Stores string in reverse order
+ * @args: Argument of type string
+ * @buffer: Buffer where the string will be stored
+ *
+ * Return: Nothing
+ */
+void store_rev(va_list args, char *buffer)
+{
+        char *str;
+        char *str_rev;
+        int str_len, i, j;
+
+        str = va_arg(args, char *);
+        str_len = _strlen(str);
+        str_rev = malloc((str_len + 1) * sizeof(char));
+
+        for (i = str_len - 1, j = 0; i >= 0; j++, i--)
+        {
+                str_rev[j] = str[i];
+        }
+        str_rev[j] = '\0';
+
+        for (i = 0; str_rev[i] != '\0'; i++)
+        {
+                *buffer = str_rev[i];
+                buffer++;
+        }
+
+        free(str_rev);
+}
