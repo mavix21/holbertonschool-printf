d8fa1af7fd87e8ddac47baa0180eaed5901e9d48
mavix21 <marcevizcarra@hotmail.com>
2022-11-13 12:08:57 -0800

    Fix indentation

--- get_formaters.c
+++ get_formaters.c
@@ -6,36 +6,36 @@
  * @format: Format string
  *
  * Return:
- *        Pointer to string containing all the formaters
- *        NULL otherwise
+ *	Pointer to string containing all the formaters
+ *	NULL otherwise
  */
 char *get_formaters(const char *format)
 {
-        int pos;
-        char *formaters;
+	int pos;
+	char *formaters;
 
-        /* Gets the position of the first letter or percent sign */
-        pos = pos_first_ltr_or_perc(format);
-        if (pos < 0)
-                return NULL;
+	/* Gets the position of the first letter or percent sign */
+	pos = pos_first_ltr_or_perc(format);
+	if (pos < 0)
+		return (NULL);
 
-        if (is_specificator(format[pos]))
-        {
-                formaters = malloc((pos + 2) * sizeof(char));
-                formaters = _strncpy(formaters, format, pos + 1);
-                return (formaters);
-        }
+	if (is_specificator(format[pos]))
+	{
+	formaters = malloc((pos + 2) * sizeof(char));
+		formaters = _strncpy(formaters, format, pos + 1);
+		return (formaters);
+	}
 
-        if (format[pos] == 'l' || format[pos] == 'h')
-        {
-                if (is_specificator(format[pos + 1]))
-                {
-                        formaters = malloc((pos + 3) * sizeof(char));
-                        formaters = _strncpy(formaters, format, pos + 2);
-                        return (formaters);
-                }
-                return (NULL);
-        }
+	if (format[pos] == 'l' || format[pos] == 'h')
+	{
+		if (is_specificator(format[pos + 1]))
+		{
+			formaters = malloc((pos + 3) * sizeof(char));
+			formaters = _strncpy(formaters, format, pos + 2);
+			return (formaters);
+		}
+		return (NULL);
+	}
 
-        return (NULL);
+	return (NULL);
 }
