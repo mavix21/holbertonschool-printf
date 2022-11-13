43e28d735c71d3d160f469dc4c227c6d79fee833
mavix21 <marcevizcarra@hotmail.com>
2022-11-13 14:44:25 -0800

    Add print_buffer function

---
+++ print_buffer.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_buffer - Prints the buffer to standard output
+ * @buffer: Buffer
+ * @buffer_length: Number of bytes to be printed
+ *
+ * Return: Nothing
+ */
+void print_buffer(char *buffer, unsigned int buffer_length)
+{
+	write(1, buffer, buffer_length);
+	free(buffer);
+}
