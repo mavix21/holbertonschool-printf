e12798512035d70af0659070560dc5ba966be98b
jose tirado <joca-001@hotmail.com>
2022-11-13 14:26:12 -0800

    add stores digits of int

---
+++ numeric_storers_01.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * store_int - Stores digits of int argument in buffer
+ * @args: Argument of type int
+ * @buffer: Buffer where the digits, including the minus sign, will be stored
+ * @length: Length modifier
+ *
+ * Return: Nothing
+ */
+void store_int(va_list args, char *buffer, int length)
+{
+        char *str_digits; 
+        int pos, str_len;
+
+        str_digits = itoa_signed(args, length);
+        str_len = _strlen(str_digits);
+
+        for (pos = str_len - 1; pos >= 0; pos--)
+                *buffer++ = str_digits[pos];
+
+        free(str_digits);
+}
