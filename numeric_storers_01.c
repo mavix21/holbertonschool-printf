50e305f85cc275d847d09f81b7b09faabf18d61b
jose tirado <joca-001@hotmail.com>
2022-11-13 14:28:52 -0800

    add unsigned int

--- numeric_storers_01.c
+++ numeric_storers_01.c
@@ -21,3 +21,27 @@ void store_int(va_list args, char *buffer, int length)
 
         free(str_digits);
 }
+
+/**
+ * store_unsgnd - Stores digits of unsigned int argument in buffer
+ * @args: Argument of type unsigned int
+ * @buffer: Buffer where the digits will be stored
+ * @length: Length modifier
+ *
+ * Return: Nothing
+ */
+void store_usgnd(va_list args, char *buffer, int length)
+{
+        char *str_digits;
+        int pos, str_len;
+        int base = 10;
+
+        str_digits = itoa_unsigned(args, length, base);
+        str_len = _strlen(str_digits);
+        
+        for (pos = str_len - 1; pos >= 0; pos--)
+                *buffer++ = str_digits[pos];
+
+        free(str_digits);
+}
+
