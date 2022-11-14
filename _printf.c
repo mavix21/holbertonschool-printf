2901b7f8ef405b426e5ced11fa11e17096ed8ee4
mavix21 <marcevizcarra@hotmail.com>
2022-11-14 06:48:57 -0800

    Fix typo

--- _printf.c
+++ _printf.c
@@ -10,15 +10,19 @@ int _printf(const char *format, ...)
 {
 	int number_of_char_printed;
 	va_list args;
+
 	/* Set a local buffer of 1024 chars and fill it out with nulls */
-	char *buffer = memset_checked(1024 * sizeof(char), '\0');
+	char *buffer = _calloc(1024, sizeof(char));
 
 	/* Validate format */
 	if (format == NULL)
 		return (-1);
 
 	va_start(args, format);
+
+	/* Start traversing the format string */
 	number_of_char_printed = traverse_format(format, args, buffer);
+
 	va_end(args);
 
 	if (number_of_char_printed >= 0)
