681cdb609f49825612adf63502636417090086ac
mavix21 <marcevizcarra@hotmail.com>
2022-11-14 06:49:59 -0800

    Handle max size

--- check_buffer_length.c
+++ check_buffer_length.c
@@ -12,11 +12,14 @@ char *check_buffer(char *buffer, int buffer_length, int n)
 {
 	char *new_buffer;
 
-	if ((buffer_length + n) >= 1023)
+	if (buffer_length >= 1024)
+		buffer_length -= 1023;
+
+	if ((buffer_length + n) >= 1024)
 	{
-		buffer -= 1023;
-		print_buffer(buffer, 1023);
-		new_buffer = memset_checked(1024 * sizeof(char), '\0');
+		buffer -= buffer_length;
+		print_buffer(buffer, buffer_length);
+		new_buffer = _calloc(1024, sizeof(char));
 		return (new_buffer);
 	}
 
