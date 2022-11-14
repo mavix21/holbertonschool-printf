1c5fd2d5223ddb1b96d44cfcb193521db4a22433
mavix21 <marcevizcarra@hotmail.com>
2022-11-13 18:55:01 -0800

    Modify return values for some functions

--- main.h
+++ main.h
@@ -20,7 +20,7 @@
 typedef struct character_converters
 {
 	char specifier;
-	void (*f)(va_list, char *);
+	int (*f)(va_list, char *);
 } get_c;
 
 /**
@@ -32,7 +32,7 @@ typedef struct character_converters
 typedef struct numeric_specifiers
 {
 	char specifier;
-	void (*f)(va_list, char *, int);
+	int (*f)(va_list, char *, int);
 } get_n;
 
 /* Main Printf Function */
@@ -50,26 +50,27 @@ int is_specifier(char c);
 int _isalpha(int);
 
 char *memset_checked(unsigned int, char);
-void print_buffer(char *, unsigned int);
+void print_buffer(char *, int);
 int get_length(char);
 char *get_formaters(const char *);
-void analize_formaters(char *, va_list, char *);
+int analize_formaters(char *, va_list, char *);
 char *itoa_sgnd(va_list, int);
 char *itoa_unsgnd(va_list, int, int);
+int traverse_format(const char *, va_list, char *);
 
 /* Storing functions */
-void store_arg(char, va_list, get_c c_st[], get_n n_st[], char *, int);
-void store_c(va_list, char *);
-void store_perc(va_list, char *);
-void store_str(va_list, char *);
-void store_Str(va_list, char *);
-void store_rev(va_list, char *);
-void store_rot13(va_list, char *);
-void store_int(va_list, char *, int);
-void store_bin(va_list, char *, int);
-void store_octal(va_list, char *, int);
-void store_unsgnd(va_list, char *, int);
-void store_hex(va_list, char *, int);
-void store_Hex(va_list, char *, int);
+int store_arg(char, va_list, get_c c_st[], get_n n_st[], char *, int);
+int store_c(va_list, char *);
+int store_perc(va_list, char *);
+int store_str(va_list, char *);
+int store_Str(va_list, char *);
+int store_rev(va_list, char *);
+int store_rot13(va_list, char *);
+int store_int(va_list, char *, int);
+int store_bin(va_list, char *, int);
+int store_octal(va_list, char *, int);
+int store_unsgnd(va_list, char *, int);
+int store_hex(va_list, char *, int);
+int store_Hex(va_list, char *, int);
 
 #endif /* End of PRINTF_H */
